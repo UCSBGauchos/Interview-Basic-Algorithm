c0f1431d70b31d745074cad66f8b6860ca7c6e94
UCSBGauchos <byang@cs.ucsb.edu>
2014-05-25 22:03:46 -0700

    Heap

--- src/Heap/Test.java
+++ src/Heap/Test.java
@@ -1,9 +1,18 @@
 package Heap;
 
+import java.util.ArrayList;
+
 public class Test {
 	public static int [] heapArray = new int[8];
+	public static ArrayList<Integer> sortedList = new ArrayList<Integer>();
 	int lastIndex = 1;
 	int parentIndex = 0;
+	int currentLength = 7;
+	
+	int lastIndex2 = 1;
+	int leftChildIndex = 0;
+	int rightChildIndex = 0;
+	int biggerChildIndex = 0;
 	
 	//bigTop-heap
 	public void build(int num){
@@ -21,6 +30,42 @@ public class Test {
 		lastIndex++;
 	}
 	
+	public void getMax(){
+		int max = heapArray[1];
+		System.out.println("The max num of this heap is "+max);
+	}
+	
+	public void sort(){
+		//for each biggest element in the heap, store in a sorted list ,it is a heap sort
+		while(currentLength>=1){
+			sortedList.add(heapArray[1]);
+			heapArray[1] = heapArray[currentLength];
+			currentLength--;
+			int index = 1;
+			leftChildIndex =  index*2;
+			rightChildIndex = index*2+1;
+			if(heapArray[leftChildIndex]>=heapArray[rightChildIndex]){
+				biggerChildIndex = leftChildIndex;
+			}else if(heapArray[leftChildIndex]<heapArray[rightChildIndex]){
+				biggerChildIndex = rightChildIndex;
+			}
+			while(biggerChildIndex<=currentLength&&heapArray[biggerChildIndex]>heapArray[index]){
+				//swap them
+				int tmp = heapArray[biggerChildIndex];
+				heapArray[biggerChildIndex] = heapArray[index];
+				heapArray[index] = tmp;
+				index = biggerChildIndex;
+				leftChildIndex =  index*2;
+				rightChildIndex = index*2+1;
+				if(heapArray[leftChildIndex]>=heapArray[rightChildIndex]){
+					biggerChildIndex = leftChildIndex;
+				}else if(heapArray[leftChildIndex]<heapArray[rightChildIndex]){
+					biggerChildIndex = rightChildIndex;
+				}
+			}
+		}
+	}
+	
 	public static void main(String [] args){
 		Test t = new Test();
 		t.build(1);
@@ -30,9 +75,9 @@ public class Test {
 		t.build(5);
 		t.build(6);
 		t.build(7);
-		
-		for(int num: heapArray){
-			System.out.println(num);
+		t.sort();
+		for(int i: sortedList){
+			System.out.println(i);
 		}
 	}
 }
